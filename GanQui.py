ad122503d26e24a2d4829b4f260ba519477a3f60
GatosLoco1990 <ricardogandica@hotmail.com>
2022-10-06 14:26:42 -0500

    Correcion de archivo

--- GanQui.py
+++ GanQui.py
@@ -1,6 +1,73 @@
 from pyparsing import col
 
 
+class Vertice:
+    def __init__(self, i, h="n/a") -> None:
+        self.id = i
+        self.visitado = False
+        self.nivel = -1
+        self.padre = None
+        self.vecinos = []
+        self.dist = float('inf')
+        self.distF = float('inf')
+        self.risk = h
+
+    def agregaVecinos(self, v):
+        if not v in self.vecinos:
+            self.vecinos.append(v)
+
+
+class Grafo:
+    def __init__(self) -> None:
+        self.vertices = {}
+
+    def agregaVertice(self, v):
+
+        if v not in self.vertices:
+            self.vertices[v] = Vertice(v)
+
+    def agregarArista(self, a, b):
+
+        if a in self.vertices and b in self.vertices:
+            self.vertices[a].agregaVecinos(b)
+            self.vertices[b].agregaVecinos(a)
+
+    def aEstrella(self, a, b):
+        if a in self.vertices and b in self.vertices:
+            self.vertices[a].costo = 0
+            self.vertices[a].costoF = self.vertices[a].costo + \
+                self.vertices[a].heuristica
+
+        for v in self.vertices:
+            if v != a:
+                self.vertices[v].costo = float('inf')
+                self.vertices[v].costoF = float('inf')
+            self.vertices[v].padre = None
+
+        abierto = [a]
+
+        while (len(abierto) > 0):
+            actual = self.minimoH(abierto)
+
+            if actual == b:
+                return [self.camino(a, b), self.vertices[b].costo]
+
+            abierto.remove(actual)
+            self.vertices[actual].visitado = True
+
+            for v in self.vertices[actual].vecinos:
+                if self.vertices[v[0]].visitado == False:
+                    if self.vertices[v[0]].id not in abierto:
+                        abierto.append(v[0])
+
+                    if self.vertices[actual].costo + v[1] < self.vertices[v[0]].costo:
+                        self.vertices[v[0]].padre = actual
+                        self.vertices[v[0]
+                                      ].costo = self.vertices[v[0]].costo + v[1]
+                        self.vertices[v[0]].costoF = self.vertices[v[0]
+                                                                   ].costo + self.vertices[v[0]].heuristica
+
+
 def csv_to_dict(nombre_archivo):
     separador = ";"
     with open(nombre_archivo, encoding="utf-8") as archivo:
@@ -55,11 +122,21 @@ def print_list(list):
 
 
 def main():
+    g = Grafo()
+
     ciudads = csv_to_dict("medellin.csv")
     a = ciudads[0].get("origen")
     b = ciudads[0].get("destino")
+    g.agregaVertice(ciudads[0].get("origen"))
+    g.agregarArista(ciudads[0].get("origen"), ciudads[0].get("destino"))
     print(a)
     print(b)
+
+    for i in range(0, len(ciudads)):
+        g.agregaVertice(ciudads[i].get("origen"))
+        g.agregarArista(ciudads[i].get("origen"), ciudads[i].get("destino"))
+
+    # g.dfs()
 
 
 main()
